ed81b09f5d840c26ca9d77a7b46f3405587acd52
x <x@x.com>
2026-09-02 22:14:21 -0400

    Implementing firebase auth

--- lib/pages/frames/setup/login.dart
+++ lib/pages/frames/setup/login.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:gapper/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginFrame extends StatelessWidget {
-  /// here we are passing in a loading value
+  /// here we are passing in the `loading`toggle
   ValueNotifier<bool> loading;
   LoginFrame(this.loading);
 
@@ -22,8 +23,49 @@ class LoginFrame extends StatelessWidget {
 
 
 class LoginPage extends StatefulWidget {
+  /// here we continue to pass in the `loading`toggle
+  /// gets used via `this.widget.loading` in `State<...>`classes
   ValueNotifier<bool> loading;
   LoginPage(this.loading, {super.key});
+
+
+  /// Networking stuff
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<User?> signUp(String email, String password) async {
+    try {
+      this.loading.value = true;
+      UserCredential creds
+      = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+
+      this.loading.value = false;
+      return creds.user;
+    } on FirebaseAuthException catch (e) {
+      print("Sign up error: ${e.message}");
+      return null;
+    } on Exception catch (e) {
+      print("Exception ${e.toString()}");
+    }
+  }
+
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      this.loading.value = true;
+      UserCredential creds
+      = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+
+      this.loading.value = false;
+      return creds.user;
+    } on FirebaseAuthException catch (e) {
+      print("Sign in error: ${e.message}");
+      return null;
+    } on Exception catch (e) {
+      print("Exception ${e.toString()}");
+    }
+  }
 
   @override
   State<LoginPage> createState() => _LoginPage();
@@ -152,21 +194,39 @@ class _LoginPage extends State<LoginPage>
           onPressed: () {
 
             bool trying_login = false;
+            List<String> login_fields = List.empty(growable: true);
+            List<String> create_fields = List.empty(growable: true);
+
+            /// Get values of fields from form
             for (var field in login_form.currentState!.fields) {
-              if (field.value != "") {
+              login_fields.add(field.value);
+            }
+
+            /// If the user has not interacted with the login form assume that they are creating a new account
+            /// This loop is mainly to check if the user is trying to log-in
+            for (var field in login_fields) {
+              if (field != "") {
                 trying_login = true;
                 login_form.currentState!.validate();
-                showSnackBar(ctx, "Logging in...");
                 break;
               }
             }
 
-            if (!trying_login) {
+
+            if (trying_login) {
+              showSnackBar(ctx, "Logging in...");
+
+
+              String email = login_fields[0];
+              String password = login_fields[1];
+              widget.signIn(email, password);
+            }
+            else {
               showSnackBar(ctx, "Creating new account...");
             }
 
             //setState(() {
-              widget.loading.value = !widget.loading.value;
+            widget.loading.value = !widget.loading.value;
             //});
 
           },
