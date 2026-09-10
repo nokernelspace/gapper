effcaecf20226ba5addd9ed1a09f34b524ddd630
x <x@x.com>
2026-09-10 12:20:45 -0400

    asdasd

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
  
  class AppState {
   static final GlobalKey<NavigatorState> NAVKEY = GlobalKey<NavigatorState>();
+  static late final AppLifecycleListener LIFECYCLE;
  }
